4ac3e9f3f2262714dbff479ffdf2d724f07db0f1
vinicio Barbosa do nascimento <viniieg@icloud.com>
2024-11-18 20:59:07 -0300

    Add files via upload

---
+++ lista.py
@@ -0,0 +1,38 @@
+# lista = []
+# print (type(lista))
+# P = [50,1,5,3,4]
+# P[0] = 0
+# print (P[0])
+# print(P)
+# nota = [70,60,80,89]
+# soma = 0
+# x = 0
+
+# while x < 4:
+#     soma += nota[x]
+#     x +=1
+
+# print(f'media: {soma/x:5.2f}')
+
+# notas = [0,0,0,0,0]
+# soma = 0
+# x = 0
+
+# while x < 5 :
+#     notas[x]= float (input(f"notas{x}: "))
+#     soma += notas [x]
+#     x+=1 
+# print(f"media: {soma/x: 5.2f}")
+
+nota = [0,0,0,0,0,0,0]
+soma = 0
+x = 0
+
+while x < 7:
+    nota [x] = float (input(f" {x+1}º nota: "))
+    soma += nota [x]
+    x+=1
+print(f"media: {soma / x : 5.2f}")
+
+# numeros = [0,0,0,0,0]
+# x = 0
